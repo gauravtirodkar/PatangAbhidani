4f1014abab1ce94039e3af827f8ed7aab88fbee0
pd <gayatripatil5519@gmail.com>
2020-12-06 02:09:05 +0530

    database connection

--- user.py
+++ user.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
 app = Flask(__name__)
-
+ 
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'butterfly'
+ 
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
-    return render_template('species_details.html',user="LoggedIn")
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * from details")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('species_details.html', data=data,user="LoggedIn")
 
 @app.route('/images')
 def images_grid():
