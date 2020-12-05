91039f0bc38c32a5f50f561b83ebe3886b3078f9
gauravtirodkar <gauravtirodkar42@gmail.com>
2020-12-05 16:33:22 +0530

    specific_details

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html',user="LoggedIn")
+    return render_template('species_details.html',user="LoggedIn")
 
 if __name__ == '__main__':
     app.run(debug=True)
