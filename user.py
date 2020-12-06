0489a70f6242be56d7682f110239e85877f309df
gauravtirodkar <gauravtirodkar42@gmail.com>
2020-12-06 11:48:04 +0530

    HomePage

--- user.py
+++ user.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('index.html',user="LoggedIn")
+
+@app.route('/specsdeets')
+def specsdeets():
     return render_template('species_details.html',user="LoggedIn")
 
 @app.route('/images')
