8fd5b22f32e5ffecdb0584ed6ba6e762d6607e7d
abhijit <abhijit>
2021-04-09 17:29:28 +0530

    testing

--- user.py
+++ user.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from bs4 import BeautifulSoup
-
+from werkzeug.utils import secure_filename
+import os
+import json
+from csv import writer
+import folium
+import pandas as pd
+import branca
+import gzip
+from io import BufferedReader
+UPLOAD_FOLDER = 'C:/applications/XAMPP/htdocs/butterfly'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 """ from flask import Flask, url_for, render_template,request,session
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
@@ -10,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app) """
 app = Flask(__name__)
  
- 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -18,12 +28,16 @@ app.config['MYSQL_DB'] = 'butterfly'
  
 mysql = MySQL(app)
 
+
 @app.route('/')
 def home():
     return render_template('index.html', user="LoggedIn")
+
 @app.route('/map')
 def map():
     return render_template('Heatmap.html')
+
+
 @app.route('/specsdeets')
 def specsdeets():
     #return render_template('species_details.html',user="LoggedIn")
@@ -39,9 +53,11 @@ def specsdeets():
 
 @app.route('/images')
 def images_grid():
+    print("Hello")
     cur = mysql.connection.cursor()
     cur.execute("SELECT * from butterflydata")
     data = cur.fetchall()
+    print("data")
     cur.execute("SELECT * FROM location")
     location = cur.fetchall()
     cur.execute("SELECT DISTINCT state FROM location ")
@@ -109,6 +125,76 @@ def register():
 		db.session.commit()
 	return render_template('auth.html') """
 
+@app.route('/upload')  
+def upload():  
+    return render_template("img.html")  
+    
+@app.route('/img', methods=['GET', 'POST'])
+def img():
+    if request.method == 'POST':  
+        f = request.files['file']  
+        result = request.form
+        str1=str(f.filename)
+        lst=[str1,result['text2'],result['text3'],result['text4'],result['text5'],result['text6'],result['text7'],result['text8']]
+        print(lst)
+        with open('static/combined.csv', 'a') as f_object:
+        
+            # Pass this file object to csv.writer()
+            # and get a writer object
+            writer_object = writer(f_object)
+        
+            # Pass the list as an argument into
+            # the writerow()
+            writer_object.writerow(lst)
+        
+            #Close the file object
+            f_object.close()
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))  
+        df = pd.read_csv("static/combined.csv")
+
+
+        m = folium.Map(location=[20.593684, 78.96288],zoom_start=5,disable_3d=True)
+
+
+
+
+
+        def get_frame(url,width,height,loc,datee,sn,cb):
+            html = """ 
+                    <!doctype html>
+                <html>
+            
+                <img id="myIFrame" class="frame" width="{}px" height="{}px" src=http://localhost/butterfly/{}""".format(width,height,url) + """ frameborder="0" ></img>
+                <p>scientific name :<b>{}</b>""".format(sn)+"""<br>date : <b>{}</b>""".format(datee)+"""<br>clicked by : <b>{}</b>""".format(cb)+"""<br>location : <b>{}</b>""".format(loc)+"""</p>
+            
+            
+                <style>
+            
+                .frame {
+
+                    border: 0;
+                    
+                    overflow:hidden;
+                
+                }
+                </style>
+                </html>"""
+            return html
+        for img1,lat,lon,loc,datee,sn,cb in zip(df['img'],df['latitude'],df['longitude'],df["location"],df['date'],df['scientific name'],df['click by']):
+
+            popup = get_frame(img1,
+                            150,
+                            150,loc,datee,sn,cb)
+            iframe = branca.element.IFrame(html=popup,width=200,height=200)
+            popup = folium.Popup(iframe,max_width=200)
+            marker = folium.Marker([lat,lon],
+                                        popup=popup)
+
+            marker.add_to(m)
+
+        m.save("templates/Heatmap.html")
+
+        return render_template("Heatmap.html", name = f.filename)  
 
 
 if __name__ == '__main__':
