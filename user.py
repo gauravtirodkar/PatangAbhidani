78095c813924bc11c9e19b821b56aed3ef4a0dd9
abhijit <abhijit>
2021-04-09 13:07:44 +0530

    testing

--- user.py
+++ user.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from bs4 import BeautifulSoup
 
+""" from flask import Flask, url_for, render_template,request,session
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app) """
 app = Flask(__name__)
  
  
@@ -65,6 +71,45 @@ def updateTable ():
 @app.route('/addData')
 def addData():
     return render_template('addData.html', user="LoggedIn")
+
+
+""" class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80))
+	username = db.Column(db.String(80), unique=True)
+	password = db.Column(db.String(80))
+
+	def __init__(self, name, username, password):
+		self.name = name
+		self.username = username
+		self.password = password """
+
+@app.route('/auth')
+def auth():
+	return render_template('layout/auth.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		name = request.form['username']
+		passw = request.form['password']
+		try:
+			data = User.query.filter_by(username=name, password=passw).first()
+		except:
+			return "Incorrect Login"
+	return render_template('layout/auth.html')
+
+""" @app.route('/register', methods=['POST'])
+def register():
+	if request.method == 'POST':
+		new_user = User(name=request.form['name'], username=request.form['username'], password=request.form['password'])
+		db.session.add(new_user)
+		db.session.commit()
+	return render_template('auth.html') """
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
